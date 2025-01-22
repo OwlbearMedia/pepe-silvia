a917134a8c98136fd77cd1ca064069b2ad06fa19
Dylan Whitney <thedylanwhitney@gmail.com>
2025-01-21 23:29:02 -0600

    Handling multiple nodes

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
-    return "Hello, World!"
+    return movies
 
 if __name__ == '__main__':
     app.run(port=5328)
--- app/ui/board/node.tsx
+++ app/ui/board/node.tsx
@@ -2,33 +2,101 @@
 
 import { useRef } from 'react';
 import styles from '@/app/ui/board/node.module.css';
+import Image from 'next/image';
+
+interface ConspiracyNode {
+  top: number;
+  left: number;
+  title: string;
+  image?: string;
+}
+
+const conspiracyNode: ConspiracyNode[] = [{
+  top: 50,
+  left: 60,
+  title: 'Pepe',
+  // image: String,
+}, {
+  top: 150,
+  left: 160,
+  title: 'Silvia',
+  // image: String,
+}];
 
 export default function Node() {
-  const nodeRef = useRef<HTMLDivElement>(null);
+  const nodeRef = useRef<Map<ConspiracyNode, HTMLDivElement | null>>(null);
+  let activeNode: HTMLDivElement | null | undefined;
+
+  function getMap() {
+    if (!nodeRef.current) {
+      // Initialize the Map on first usage.
+      nodeRef.current = new Map();
+    }
+    return nodeRef.current;
+  }
 
   function elementDrag(e: MouseEvent) {
     e.preventDefault();
-    if (nodeRef.current) {
-      nodeRef.current.style.setProperty('--top', `${nodeRef.current.offsetTop + e.movementY}px`)
-      nodeRef.current.style.setProperty('--left', `${nodeRef.current.offsetLeft + e.movementX}px`)
+    if (activeNode) {
+      activeNode.style.setProperty('--top', `${activeNode.offsetTop + e.movementY}px`)
+      activeNode.style.setProperty('--left', `${activeNode.offsetLeft + e.movementX}px`)
     }
   }
+
+  
 
   function dragEnd() {
-    if (nodeRef.current) {
-      nodeRef.current.style.setProperty('--cursor', 'grab');
+    if (activeNode) {
+      activeNode.style.setProperty('--cursor', 'grab');
+      activeNode.removeEventListener('mousemove', elementDrag);
+      activeNode.removeEventListener('mouseup', dragEnd);
     }
-    document.removeEventListener('mousemove', elementDrag);
-    document.removeEventListener('mouseup', dragEnd);
+    
   }
 
-  function click() {
-    if (nodeRef.current) {
-      nodeRef.current.style.setProperty('--cursor', 'grabbing');
+  function click(node: ConspiracyNode) {
+    const map = getMap();
+    activeNode = map?.get(node);
+    if (activeNode) {
+      activeNode.style.setProperty('--cursor', 'grabbing');
+      activeNode.addEventListener('mousemove', elementDrag, false);
+      activeNode.addEventListener('mouseup', dragEnd, false);
     }
-    document.addEventListener('mousemove', elementDrag, false);
-    document.addEventListener('mouseup', dragEnd, false);
   }
 
-  return <div ref={nodeRef} className={styles.node} onMouseDown={click}>Node</div>;
+  const nodes = conspiracyNode.map((node: ConspiracyNode, index) => {
+    return (
+      <div
+        key={index}
+        ref={(n) => {
+          const map = getMap();
+          map.set(node, n);
+
+          return () => {
+            map.delete(node);
+          };
+        }}
+        style={{
+          top: 'var(--top)',
+          left: 'var(--left)',
+          // '--left': `${node.left}px`,
+          // '--top': `${node.top}px`,
+        }}
+        className={styles.node}
+        onMouseDown={() => click(conspiracyNode[index])}
+      >
+        <div className={styles.header}>
+          <Image
+            src={'/tack.png'}
+            className={styles.tack}
+            width={50}
+            height={50}
+            alt="Tack"
+          />
+        </div>
+      </div>
+    )
+  });
+
+  return (<div>{nodes}</div>);
 }
