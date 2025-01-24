e56a167ee340af55cd0af0c3d6d208a0bf6d77aa
Dylan Whitney <thedylanwhitney@gmail.com>
2025-01-23 21:42:01 -0600

    Fixing build

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
               Your preimier conspiracy board app.
             </p>
             <p className="mb-5">
-              Have you stumbled on a major company conspiracy? Are you seeing a lot of mail for a co-worker only go to their office and find they don't exist? So you go to Carol in HR only to find out there is no Carol. That in fact, half the employees in the company are made up, that the office is a ghost town?
+              Have you stumbled on a major company conspiracy? Are you seeing a lot of mail for a co-worker only go to their office and find they don&apos;t exist? So you go to Carol in HR only to find out there is no Carol. That in fact, half the employees in the company are made up, that the office is a ghost town?
             </p>
             <p className="mb-5">
               Make your own conspiracy board and share it with the world!
