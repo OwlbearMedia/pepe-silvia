f090879032590f69aa7275d222517c264c214d4f
Dylan Whitney <thedylanwhitney@gmail.com>
2025-01-29 11:34:32 -0600

    updating url

--- app/lib/api.tsx
+++ app/lib/api.tsx
@@ -1,4 +1,4 @@
-const baseUrl = 'http://ec2-44-242-150-60.us-west-2.compute.amazonaws.com'
+const baseUrl = 'https://server-of-the-outer-gods.com'
 
 export async function fetchConspiracyBoard(boardId: number) {
   const response = await fetch(`${baseUrl}/api/board/${boardId}`, {
